e26a43a2e62e35a169c8a3697ae305e5b60d4fea
Matheus Duarte <mtduarteadm@hotmail.com>
2021-05-01 21:14:45 -0300

    Semana 6 - Funções em Dart
    Semana 6 - Funções em Dart

---
+++ Semana 06/atividade06.dart
@@ -0,0 +1,16 @@
+void main() {
+  
+  pets("cachorro", nome: "totó");
+}
+
+void pets (String animal, {nome, raca, idade}){
+  
+  print('Bemvindo ao Petshop!');
+  print('Seu bichinho é um:\n$animal');
+  print('Seu nome é:');
+  print(nome ?? "desconhecido");
+  print('A raça do bichinho é:');
+  print(raca ?? "desconhecida");
+  print('Sua idade:');
+  print(idade ?? 0);
+}
