fbfdd6c461d34abadc61792d59c5d6be46e3d5fc
Matheus Duarte <mtduarteadm@hotmail.com>
2021-06-05 15:29:26 -0300

    Atividade 11 - Navegações Rotas
    Atividade 11 - Navegações Rotas

---
+++ Semana 11/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Navegation Basics",
+    initialRoute: '/primeira',
+    routes: {
+      '/primeira': (context) => FirstRoute(),
+      '/segunda': (context) => SecondRoute(),
+    },
+    theme: ThemeData(
+      primarySwatch: Colors.blueGrey,
+    ),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Primeira Pagina'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Abrir segunda rota'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/segunda');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Matheus Augusto Duarte Silva"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Retornar!'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/primeira');
+          },
+        ),
+      ),
+    );
+  }
+}
+
---
+++ Semana 11/main_rotasputpush.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Navegation Basics",
+    home: FirstRoute(),
+    theme: ThemeData(
+    primarySwatch: Colors.blueGrey,
+    ),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Primeira Pagina'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Abrir segunda rota'),
+          onPressed: () {
+            Navigator.push(context,MaterialPageRoute(builder: (context) => SecondRoute()),);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Matheus Augusto Duarte Silva"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Retornar!'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
+
