2f12f9bfb8cf7d11e7b32aa1a119c9653ac5dadf
Matheus Duarte <mtduarteadm@hotmail.com>
2021-06-11 22:49:02 -0300

    Atividade 12 - Padrao MVC
    Atividade 12 - Padrao MVC

---
+++ Semana 12/main.dart
@@ -0,0 +1,22 @@
+import 'package:atividade12/view/myhomepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: MyHomePage(title: 'Home'),
+    );
+  }
+}
+
